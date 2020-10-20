6748ad79cc1f86babef38dbe1a3fcb0787817ad8
Basnor <basnor@mail.ru>
2020-10-21 01:15:38 +0700

    add frontend app structure

---
+++ frontend/src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from 'react';
+import ReactDOM from 'react-dom';
+
+import Header from './layout/Header';
+
+class App extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Header />
+                <h1>Hello world!!!!</h1>
+            </Fragment>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ frontend/src/components/layout/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+    render() {
+        return (
+            <div>
+                <p>Events</p>
+                <nav>
+                    <ul>
+                        <li><a href="#">Hello1</a></li>
+                        <li><a href="#">Hello2</a></li>
+                    </ul>
+                </nav>
+                
+            </div>
+        )
+    }
+}
+
+export default Header

3ecceeb44e5f39db43526b0ec82a5c34c61a1672
Basnor <basnor@mail.ru>
2020-10-20 19:47:39 +0700

    added admin with basic event

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Event
+
 
 # Register your models here.
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'keyWord',  'description', 'startData', 'endData')
+    list_display_links = ('id', 'title')
+
+
+admin.site.register(Event, EventAdmin)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=250)
+    keyWord = models.CharField(max_length=25)
+    description = models.CharField(max_length=500, blank=True)
+    startData = models.DateTimeField()
+    endData = models.DateTimeField()
+
+    def __str__(self):
+        return self.title

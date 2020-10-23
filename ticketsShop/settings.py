e398a6385c42da763a9a9a3690657974c97b4eb0
Basnor <basnor@mail.ru>
2020-10-23 14:55:06 +0700

    replaced sqlite to mysql

--- ticketsShop/settings.py
+++ ticketsShop/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'ticketsShop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodb',
+        'USER': 'root',
+        'PASSWORD': 'mysql',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 

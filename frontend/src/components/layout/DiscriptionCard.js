067894fd4a7c144c4b07dcbdbbb46e2c6fd3e5dd
Basnor <basnor@mail.ru>
2020-12-22 22:53:23 +0700

    add styles to event page

--- frontend/src/components/layout/DiscriptionCard.js
+++ frontend/src/components/layout/DiscriptionCard.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react'
+import Grid from '@material-ui/core/Grid';
 
-//import "./card.scss" 
+import "./card.scss" 
 
 export class DiscriptionCard extends Component {
     render() {
         return (
             <div className='card'>
-                <h3>{this.props.title}</h3>
-                <span>  
-                    {new Intl.DateTimeFormat("ru").format(this.props.start)}
-                    &nbsp;-&nbsp;
-                    {new Intl.DateTimeFormat("ru").format(this.props.end)}
-                </span>
-                <p>{this.props.description}</p>
-                <img src={this.props.image} alt=""></img>              
+                <Grid container spacing={4} justify="center" alignItems="center">
+                    <Grid item xs={12} sm={6}>
+                        <div className='card__info'>
+                            <h3>{this.props.title}</h3>
+                            <span>  
+                                {new Intl.DateTimeFormat("ru").format(this.props.start)}
+                                &nbsp;-&nbsp;
+                                {new Intl.DateTimeFormat("ru").format(this.props.end)}
+                            </span>
+                            <p>{this.props.description}</p>
+                        </div>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <img src={this.props.image} alt=""></img>         
+                    </Grid>    
+                </Grid>  
             </div>
         )
     }
